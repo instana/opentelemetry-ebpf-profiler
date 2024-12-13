735776405f5cd56bb20d724fe74340d7b1eb6682
Teresa Noviello <Teresa.Noviello@ibm.com>
2024-12-13 11:09:13 -0800

    Migrate the build on redhat ubi-minimal 8, QA
    Migrate the build on redhat ubi-minimal 8.
    Attention: Clang version on this image is 18, not 16.
    Isolate docker files in directory docker-image.
    Introduce an entrypoint for the docker image, to better deal with the GO
    environment.
    Dockerfile: install GolangCi deps in the docker image, copy the binaries
    in the container's workdir in /agent/go/bin.
    Makefile: fix the clean target.
    Makefiles: substitute clang-16 with clang.
    Makefile: remove the installation of GolangCi deps during linting.
    Makefile: add targets for GO deps management
    Makefile: pin GO deps versions
    Makefile: pin porto version to "v0.6.0" (instead of latest).
    The latest version of porto requires GO 1.23.
    Makefile: improve target linter-version, print all GolangCi deps
    versions.
    GO: touch go/bin/.gitkeep, the directory bin contains the GO deps

---
+++ docker-image/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ORIG_GOPATH="/agent-go"
+
+NEW_GOPATH="/agent/go"
+GOPATH="$NEW_GOPATH"
+GOCACHE="$GOPATH/.cache"
+GOBIN="$GOPATH/bin"
+PATH="$PATH:$GOBIN"
+GOLANGCI_LINT_CACHE=$GOCACHE
+
+# Check if /agent/go exists, and create it if not
+if [ ! -d "${GOPATH}" ]; then
+  mkdir -p ${GOPATH}
+  mkdir -p ${GOBIN}
+fi
+
+cp --recursive $ORIG_GOPATH/bin/* $GOBIN
+
+export GOPATH
+export GOCACHE
+export GOBIN
+export PATH
+export GOLANGCI_LINT_CACHE
+
+# Run the actual command (e.g., bash or other processes)
+exec $@
