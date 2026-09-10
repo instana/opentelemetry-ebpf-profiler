e5daecd4f7202569024f104996cc555742a034bd
SaiKiran <152687049+SaiKiranKonchada@users.noreply.github.com>
2026-09-10 18:51:21 +0530

    Add cargo to Dockerfile (#25)

--- docker-image/entrypoint.sh
+++ docker-image/entrypoint.sh
@@ -23,6 +23,24 @@ export GOBIN
 export PATH
 export GOLANGCI_LINT_CACHE
 
+# Rust/cargo environment
+CARGO_HOME="/usr/local/cargo"
+RUSTUP_HOME="/usr/local/rustup"
+
+# Check if cargo/rustup dirs exist, and create them if not
+if [ ! -d "${CARGO_HOME}" ]; then
+  mkdir -p ${CARGO_HOME}/bin
+fi
+if [ ! -d "${RUSTUP_HOME}" ]; then
+  mkdir -p ${RUSTUP_HOME}
+fi
+
+PATH="$PATH:$CARGO_HOME/bin"
+
+export CARGO_HOME
+export RUSTUP_HOME
+export PATH
+
 git config --global --add safe.directory /agent
 
 # Run the actual command (e.g., bash or other processes)
